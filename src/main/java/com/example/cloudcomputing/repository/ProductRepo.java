033247a7fce480f503b6f693c39565c063465f37
Anh Hoang The <hoangtheanhc2lp@gmail.com>
2023-12-14 19:04:54 +0700

    update

--- src/main/java/com/example/cloudcomputing/repository/ProductRepo.java
+++ src/main/java/com/example/cloudcomputing/repository/ProductRepo.java
@@ -1,6 +1,7 @@
 package com.example.cloudcomputing.repository;
 
 import com.example.cloudcomputing.dto.Product;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
